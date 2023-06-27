b01e6731e5e56f2eaa89ce1a4d0cccfce100279e
MOUMENE-Yassine <moumene.yassineus@gmail.com>
2023-06-26 17:41:36 -0700

    4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,5 +16,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print('out of range')
             result = 0
         finally:
-            length .append(result)
+            length.append(result)
     return length
