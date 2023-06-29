ea40f7ebbb8d3a76bd160a8cc5173fbb42939997
MOUMENE-Yassine <moumene.yassineus@gmail.com>
2023-06-29 16:00:34 -0700

    6. Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,7 +7,17 @@ class Square:
 
     def __init__(self, n=0, position=(0, 0)):
         """class constructor"""
+        if not isinstance(n, int):
+            raise TypeError('size must be an integer')
+        if n < 0:
+            raise ValueError('size must be >= 0')
         self.__size = n
+        if not isinstance(position, tuple) or len(position) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if type(position[0]) is not int or type(position[-1]) is not int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if position[0] < 0 or position[-1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = position
 
     def area(self):
@@ -34,9 +44,11 @@ class Square:
 
     @position.setter
     def position(self, position):
-        if (not isinstance(position, tuple) or len(position) != 2 or
-            type(position[0]) is not int or type(position[-1]) is not int or
-            position[0] < 0 or position[-1] < 0):
+        if not isinstance(position, tuple) or len(position) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if type(position[0]) is not int or type(position[-1]) is not int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if position[0] < 0 or position[-1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = position
 
