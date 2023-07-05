7f313f1f2c0d946c4dff037fe73bb6f55084c750
MOUMENE-Yassine <moumene.yassineus@gmail.com>
2023-07-04 17:43:47 -0700

    30. Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """prevents the user from dynamically creating"""
 
+
 class LockedClass():
     """prevents the user from dynamically attributes creating"""
     __slots__ = ['first_name']
 
     def __init__(self):
-        
+        """Construction"""
         pass
