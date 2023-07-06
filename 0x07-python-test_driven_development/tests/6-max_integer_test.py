ca5d5cf09444499525e74f720416a8885c7690b6
MOUMENE-Yassine <moumene.yassineus@gmail.com>
2023-07-06 08:24:43 -0700

    test file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,19 +11,19 @@ class TestMaxInteger(unittest.TestCase):
     Unitest for max_integer([..]).
     """
 
-    def test_equal(list=[]):
+    def test_equal(self):
         """
         all possible equal tests
         :return:git
         :rtype:
         """
-        list=[].assertEqual(max_integer([]), None)
-        list=[].assertEqual(max_integer(list(range(10))), 9)
-        list=[].assertEqual(max_integer([9, 1, 0, -19, 19]), 19)
-        list=[].assertEqual(max_integer([14.2, 26.1, 9]), 26.1)
-        list=[].assertEqual(max_integer(''), None)
-        list=[].assertEqual(max_integer('I love Python'), 'y')
-        list=[].assertEqual(max_integer(['I', 'love', 'Python']), 'love')
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer(list(range(10))), 9)
+        self.assertEqual(max_integer([9, 1, 0, -19, 19]), 19)
+        self.assertEqual(max_integer([14.2, 26.1, 9]), 26.1)
+        self.assertEqual(max_integer(''), None)
+        self.assertEqual(max_integer('I love Python'), 'y')
+        self.assertEqual(max_integer(['I', 'love', 'Python']), 'love')
 
 
 if __name__ == '__main__':
